3af9fe7e2b45c83d4b3e3ccf14f424ecd71f4627
Bastien Zalugas <bastien.zalugas@hotmail.fr>
2021-04-18 12:22:28 +0200

    Ajout qq commentaires dans App.js

--- trunk/bookerist-front/src/App.js
+++ trunk/bookerist-front/src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
     });
   }
 
-
+  //Quand le composant est monté, il récupère l'état sauvegardé s'il existe
   componentDidMount() {
     let user = window.localStorage.getItem('userInfos');
     if (user != null){
@@ -41,15 +41,18 @@ class App extends Component {
     }
   }
 
+  //Chaque fois que le composant est mis à jour, on enregistre l'état dans le stockage local
   componentDidUpdate() {
     let user = this.state;
     window.localStorage.setItem('userInfos', JSON.stringify(user));
   }
 
+  //Utilisée lors de la connexion pour enregistrer directement les informations dans l'état
   setUserInfos = (prenom, nom, mail) => {
     this.setState({ prenom, nom, mail });
   }
 
+  //fonction permettant de remettre le state à 0 (lors d'une déconnexion par exemple)
   clearState = () => {
     this.setState({
       menuOpened: false,
@@ -67,6 +70,7 @@ class App extends Component {
     })
   }
 
+  //Fonction pour gérer le status de l'utilisateur (connecté ou non)
   handleLoginChange = (loggedIn) => {
     this.setState({ loggedIn });
     if (!loggedIn){
@@ -76,6 +80,7 @@ class App extends Component {
     }
   }
 
+  //Fonction qui gère le changement de pages
   handlePage = (pages) => {
     //tableau contenant toutes les props éventuelles de pages
     const tab = ["inAccueil", "inInscription", "inForgotPass", "inContact"];
@@ -100,6 +105,7 @@ class App extends Component {
     }
   }
   
+  //Quand le composant va être démonté, on s'assure de vider le stockage local
   componentWillUnmount() {
     localStorage.clear();
   }
